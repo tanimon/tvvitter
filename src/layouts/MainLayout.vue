fe4fd497eb6424a4a5d064aa66fb97699267ae43
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-10 16:10:12 +0900

    Update MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,26 +1,92 @@
 <template>
   <q-layout view="lHr lpR fFf">
-    <q-header bordered class="bg-primary text-white">
+    <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+        <q-toolbar-title class="text-weight-bold">Tvvitter</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      :width="283"
+      bordered
+    >
+      <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">
+            About
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
+      <q-input
+        class="q-pa-md"
+        rounded
+        outlined
+        dense
+        v-model="search"
+        label="Search Tvviter"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list padding separator>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-grey" overline>Education</q-item-label>
+            <q-item-label class="text-weight-bold">
+              Something amazing happened!
+            </q-item-label>
+            <q-item-label caption>
+              Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-grey" overline>Education</q-item-label>
+            <q-item-label class="text-weight-bold">
+              Something amazing happened!
+            </q-item-label>
+            <q-item-label caption>
+              Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-grey" overline>Education</q-item-label>
+            <q-item-label class="text-weight-bold">
+              Something amazing happened!
+            </q-item-label>
+            <q-item-label caption>
+              Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
