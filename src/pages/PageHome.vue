e30a1cbfae5f76d3dde49b8b25d5d8b847c1c06f
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-14 16:13:23 +0900

    Add a template of tweets list

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -31,6 +31,37 @@
     </div>
 
     <q-separator class="divider" color="grey-1" size="10px" />
+
+    <q-list class="q-py-md">
+      <q-item clickable>
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img src="https://avatars.githubusercontent.com/u/8575113?v=4" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>たにモン</strong>
+            <span class="text-grey-7"> @tanimon_dev ・1 min ago </span>
+          </q-item-label>
+          <q-item-label class="tvveet-content text-body1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+            repellat voluptatem ratione provident, porro necessitatibus
+            perferendis ullam minima exercitationem pariatur dolorem sint hic
+            placeat unde! Ipsam ut possimus magnam rem.
+          </q-item-label>
+          <div class="tvveet-icons row justify-between">
+            <q-btn flat round size="sm" color="grey" icon="far fa-comment" />
+            <q-btn flat round size="sm" color="grey" icon="fas fa-retweet" />
+            <q-btn flat round size="sm" color="grey" icon="far fa-heart" />
+            <q-btn flat round size="sm" color="grey" icon="fas fa-trash" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+    </q-list>
   </q-page>
 </template>
 
@@ -54,4 +85,8 @@ export default {
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-3
+.tvveet-content
+  white-space: pre-line
+.tvveet-icons
+  margin-left: -5px
 </style>
