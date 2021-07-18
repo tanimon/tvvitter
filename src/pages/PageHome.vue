c2df6da2892ffef792b33519c4d5f6bcfb3a01f1
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-18 16:15:24 +0900

    Add q-scroll-area element
    Make the left drawer not to disappear when scrolling down tvveets list

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,85 +1,103 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-gutter-md">
-      <div class="col">
-        <q-input
-          v-model="newTvveetContent"
-          class="new-tvveet"
-          placeholder="What's happening?"
-          maxlength="280"
-          bottom-slots
-          counter
-          autogrow
-        >
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://avatars.githubusercontent.com/u/8575113?v=4" />
-            </q-avatar>
-          </template>
-        </q-input>
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-gutter-md">
+        <div class="col">
+          <q-input
+            v-model="newTvveetContent"
+            class="new-tvveet"
+            placeholder="What's happening?"
+            maxlength="280"
+            bottom-slots
+            counter
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img
+                  src="https://avatars.githubusercontent.com/u/8575113?v=4"
+                />
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink q-mb-lg">
+          <q-btn
+            @click="addNewTvveet"
+            :disable="!newTvveetContent"
+            unelevated
+            rounded
+            no-caps
+            color="primary"
+            label="Tvveet"
+          />
+        </div>
       </div>
-      <div class="col col-shrink q-mb-lg">
-        <q-btn
-          @click="addNewTvveet"
-          :disable="!newTvveetContent"
-          unelevated
-          rounded
-          no-caps
-          color="primary"
-          label="Tvveet"
-        />
-      </div>
-    </div>
 
-    <q-separator class="divider" color="grey-1" size="10px" />
+      <q-separator class="divider" color="grey-1" size="10px" />
 
-    <q-list separator>
-      <transition-group
-        appear
-        enter-active-class="animated fadeIn slow"
-        leave-active-class="animated fadeOut slow"
-      >
-        <q-item
-          v-for="tvveet in tvveets"
-          :key="tvveet.id"
-          clickable
-          class="q-py-md"
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn slow"
+          leave-active-class="animated fadeOut slow"
         >
-          <q-item-section avatar top>
-            <q-avatar size="xl">
-              <img src="https://avatars.githubusercontent.com/u/8575113?v=4" />
-            </q-avatar>
-          </q-item-section>
+          <q-item
+            v-for="tvveet in tvveets"
+            :key="tvveet.id"
+            clickable
+            class="q-py-md"
+          >
+            <q-item-section avatar top>
+              <q-avatar size="xl">
+                <img
+                  src="https://avatars.githubusercontent.com/u/8575113?v=4"
+                />
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label class="text-subtitle1">
-              <strong>{{ tvveet.displayName }}</strong>
-              <span class="text-grey-7">
-                @{{ tvveet.username }} &bull; {{ relativeDate(tvveet.date) }}
-              </span>
-            </q-item-label>
-            <q-item-label class="tvveet-content text-body1">
-              {{ tvveet.content }}
-            </q-item-label>
-            <div class="tvveet-icons row justify-between">
-              <q-btn flat round size="sm" color="grey" icon="far fa-comment" />
-              <q-btn flat round size="sm" color="grey" icon="fas fa-retweet" />
-              <q-btn flat round size="sm" color="grey" icon="far fa-heart" />
-              <q-btn
-                @click="deleteTvveet(tvveet)"
-                flat
-                round
-                size="sm"
-                color="grey"
-                icon="fas fa-trash"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong>{{ tvveet.displayName }}</strong>
+                <span class="text-grey-7">
+                  @{{ tvveet.username }} &bull; {{ relativeDate(tvveet.date) }}
+                </span>
+              </q-item-label>
+              <q-item-label class="tvveet-content text-body1">
+                {{ tvveet.content }}
+              </q-item-label>
+              <div class="tvveet-icons row justify-between">
+                <q-btn
+                  flat
+                  round
+                  size="sm"
+                  color="grey"
+                  icon="far fa-comment"
+                />
+                <q-btn
+                  flat
+                  round
+                  size="sm"
+                  color="grey"
+                  icon="fas fa-retweet"
+                />
+                <q-btn flat round size="sm" color="grey" icon="far fa-heart" />
+                <q-btn
+                  @click="deleteTvveet(tvveet)"
+                  flat
+                  round
+                  size="sm"
+                  color="grey"
+                  icon="fas fa-trash"
+                />
+              </div>
+            </q-item-section>
+          </q-item>
 
-        <q-separator />
-      </transition-group>
-    </q-list>
+          <q-separator />
+        </transition-group>
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
